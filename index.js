ffc5cd64fd416baab3cd84c027d9e57efd64811f
Jerry <jerlee.evans@gmail.com>
2023-09-21 21:19:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+console.log(uuidv4());
+
+
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader ('Content-type', 'text/html');
+//     res.end('<h2>Lorem  Lorem    LoreM     </h2>');
+// });
+
+// server.listen(5000, '127.0.1.1', () => {
+//     console.log('SerVer Runnin....');
+// });
